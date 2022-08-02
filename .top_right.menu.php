24246de05b0c68423c1b6db8a2e4c65b6730ea29
nekoma-02 <kaineko01@gmail.com>
2022-08-02 14:30:22 +0300

    lassie shop first commit

---
+++ .top_right.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"О компании", 
+		"about/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Оплата", 
+		"", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Доставка", 
+		"", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
